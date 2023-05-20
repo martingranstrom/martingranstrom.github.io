0eb5a10c5cee0734ac4ed930b1f05054bf30ee81
Martin Granström Ivancic <martingranstrom@live.se>
2023-05-20 23:56:31 +0200

    folders

--- src/App.js
+++ src/App.js
@@ -1,197 +1,13 @@
 import React, { useState } from "react";
 import "./App.css";
-import logo from "./logo.svg";
-import azureLogo from "./azureLogo.svg";
-import cSharpLogo from "./cSharpLogo.svg";
-import cppLogo from "./cppLogo.svg";
-import jsLogo from "./jsLogo.svg";
-import blazorLogo from "./blazorLogo.png";
-import terraformLogo from "./terraformLogo.svg";
-import bashLogo from "./bashLogo.svg";
-import dockerLogo from "./dockerLogo.svg";
-import dotnetLogo from "./dotnetLogo.svg";
-import nodeLogo from "./nodeLogo.svg";
-import pythonLogo from "./pythonLogo.svg";
-import mongoDbLogo from "./mongodbLogo.svg";
-import mugshot from "./mugshot.jpg";
-import linkedinIcon from "./linkedinIcon.png";
-import phone from "./phone.png";
-import envelope from "./envelope.png";
-import useIntersect from "./useIntersect";
-
-function ShowIntersecting(entry, show) {
-  if (entry !== undefined) {
-    if (entry.target !== undefined) {
-      if (entry.isIntersecting) {
-        entry.target.classList.add(show);
-        entry.target.classList.remove("hidden");
-      } else {
-        entry.target.classList.add("hidden");
-        entry.target.classList.remove(show);
-      }
-    }
-  }
-}
-
-function GetBullets(bullets) {
-  var res = [];
-  for (var i = 0; i < bullets.length; i++) {
-    res.push(
-      <p className="bullet" key={i}>
-        {bullets[i]}
-        <span class="extra">My hidden text that contains a lot of stuff!</span>
-      </p>
-    );
-  }
-
-  return res;
-}
-
-function IntersectBoxVerticalBullets(props) {
-  const [ref, entry] = useIntersect({});
-  ShowIntersecting(entry, "show");
-  return (
-    <>
-      <div ref={ref}>
-        {props.title}
-        <div className="boxSection">{GetBullets(props.bullets)}</div>
-      </div>
-    </>
-  );
-}
-
-function IntersectBoxHorizontalBullets(props) {
-  const [ref, entry] = useIntersect({});
-  ShowIntersecting(entry, "showflex");
-  return (
-    <>
-      <div ref={ref}>
-        {props.title}
-        <p className="flexbreak"></p>
-        <span className="container">
-          <img
-            className="compBadge"
-            src={logo}
-            alt="logo"
-            width="200px"
-            height="200px"
-          />
-        </span>
-        <span className="container">
-          <img
-            className="compBadge"
-            src={azureLogo}
-            alt="logo"
-            width="130px"
-            height="130px"
-          />
-        </span>
-        <span className="container">
-          <img
-            className="compBadge"
-            src={cSharpLogo}
-            alt="logo"
-            width="123px"
-            height="123px"
-          />
-        </span>
-        <span className="container">
-          <img
-            className="compBadge"
-            src={cppLogo}
-            alt="logo"
-            width="220px"
-            height="220px"
-          />
-        </span>
-        <span className="container">
-          <img
-            className="compBadge"
-            src={jsLogo}
-            alt="logo"
-            width="150px"
-            height="150px"
-            style={{ borderRadius: "20px" }}
-          />
-        </span>
-        <p className="flexbreak"></p>
-        <span className="container">
-          <img
-            className="compBadge"
-            src={blazorLogo}
-            alt="logo"
-            width="180px"
-            height="180px"
-          />
-        </span>
-        <span className="container">
-          <img
-            className="compBadge"
-            src={terraformLogo}
-            alt="logo"
-            width="200px"
-            height="200px"
-          />
-        </span>
-        <span className="container">
-          <img
-            className="compBadge"
-            src={bashLogo}
-            alt="logo"
-            width="200px"
-            height="200px"
-          />
-        </span>
-        <span className="container">
-          <img
-            className="compBadge"
-            src={dockerLogo}
-            alt="logo"
-            width="200px"
-            height="200px"
-          />
-        </span>
-        <span className="container">
-          <img
-            className="compBadge"
-            src={nodeLogo}
-            alt="logo"
-            width="200px"
-            height="200px"
-          />
-        </span>
-        <p className="flexbreak"></p>
-        <span className="container">
-          <img
-            className="compBadge"
-            src={pythonLogo}
-            alt="logo"
-            width="200px"
-            height="200px"
-          />
-        </span>
-        <span className="container">
-          <img
-            className="compBadge"
-            src={dotnetLogo}
-            alt="logo"
-            width="180px"
-            height="180px"
-          />
-        </span>
-        <span className="container">
-          <img
-            className="compBadge"
-            src={mongoDbLogo}
-            alt="logo"
-            width="180px"
-            height="180px"
-          />
-        </span>
-      </div>
-    </>
-  );
-}
+import mugshot from "./images/mugshot.jpg";
+import linkedinIcon from "./images/linkedinIcon.png";
+import phone from "./images/phone.png";
+import envelope from "./images/envelope.png";
+import {
+  IntersectBoxVerticalBullets,
+  IntersectBoxHorizontalBullets,
+} from "./IntersectBox.js";
 
 function App() {
   const [showContact, setShowContact] = useState(false);
@@ -273,6 +89,7 @@ function App() {
           "Collector Bank AB | 2020 - now",
           "Zenseact AB | 2017 - 2020",
         ]}
+        extras={["Collector extra", "zenuity extra"]}
       />
       <IntersectBoxVerticalBullets
         title={"Skills"}
@@ -282,12 +99,14 @@ function App() {
           "tre",
           "hej",
         ]}
+        extras={[]}
       />
       <IntersectBoxVerticalBullets
         title={"Certifications"}
         bullets={[
           "International Software Testing Qualifications Board (ISTQB) - Foundation level",
         ]}
+        extras={[]}
       />
       <IntersectBoxHorizontalBullets title={"Teknisk kompetens"} />
     </>
---
+++ src/IntersectBox.js
@@ -0,0 +1,193 @@
+import React from "react";
+import "./App.css";
+import logo from "./images/logo.svg";
+import azureLogo from "./images/azureLogo.svg";
+import cSharpLogo from "./images/cSharpLogo.svg";
+import cppLogo from "./images/cppLogo.svg";
+import jsLogo from "./images/jsLogo.svg";
+import blazorLogo from "./images/blazorLogo.png";
+import terraformLogo from "./images/terraformLogo.svg";
+import bashLogo from "./images/bashLogo.svg";
+import dockerLogo from "./images/dockerLogo.svg";
+import dotnetLogo from "./images/dotnetLogo.svg";
+import nodeLogo from "./images/nodeLogo.svg";
+import pythonLogo from "./images/pythonLogo.svg";
+import mongoDbLogo from "./images/mongodbLogo.svg";
+import useIntersect from "./hooks/useIntersect";
+
+function ShowIntersecting(entry, show) {
+  if (entry !== undefined) {
+    if (entry.target !== undefined) {
+      if (entry.isIntersecting) {
+        entry.target.classList.add(show);
+        entry.target.classList.remove("hidden");
+      } else {
+        entry.target.classList.add("hidden");
+        entry.target.classList.remove(show);
+      }
+    }
+  }
+}
+
+function GetBullets(bullets, extras) {
+  var res = [];
+  for (var i = 0; i < bullets.length; i++) {
+    res.push(
+      <p className="bullet" key={i}>
+        {bullets[i]}
+        <br></br>
+        <span className="extra">{extras.length > 0 ? extras[i] : ""}</span>
+      </p>
+    );
+  }
+
+  return res;
+}
+
+export function IntersectBoxVerticalBullets(props) {
+  const [ref, entry] = useIntersect({});
+  ShowIntersecting(entry, "show");
+  return (
+    <>
+      <div ref={ref}>
+        {props.title}
+        <div className="boxSection">
+          {GetBullets(props.bullets, props.extras)}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export function IntersectBoxHorizontalBullets(props) {
+  const [ref, entry] = useIntersect({});
+  ShowIntersecting(entry, "showflex");
+  return (
+    <>
+      <div ref={ref}>
+        {props.title}
+        <p className="flexbreak"></p>
+        <span className="container">
+          <img
+            className="compBadge"
+            src={logo}
+            alt="logo"
+            width="200px"
+            height="200px"
+          />
+        </span>
+        <span className="container">
+          <img
+            className="compBadge"
+            src={azureLogo}
+            alt="logo"
+            width="130px"
+            height="130px"
+          />
+        </span>
+        <span className="container">
+          <img
+            className="compBadge"
+            src={cSharpLogo}
+            alt="logo"
+            width="123px"
+            height="123px"
+          />
+        </span>
+        <span className="container">
+          <img
+            className="compBadge"
+            src={cppLogo}
+            alt="logo"
+            width="220px"
+            height="220px"
+          />
+        </span>
+        <span className="container">
+          <img
+            className="compBadge"
+            src={jsLogo}
+            alt="logo"
+            width="150px"
+            height="150px"
+            style={{ borderRadius: "20px" }}
+          />
+        </span>
+        <p className="flexbreak"></p>
+        <span className="container">
+          <img
+            className="compBadge"
+            src={blazorLogo}
+            alt="logo"
+            width="180px"
+            height="180px"
+          />
+        </span>
+        <span className="container">
+          <img
+            className="compBadge"
+            src={terraformLogo}
+            alt="logo"
+            width="200px"
+            height="200px"
+          />
+        </span>
+        <span className="container">
+          <img
+            className="compBadge"
+            src={bashLogo}
+            alt="logo"
+            width="200px"
+            height="200px"
+          />
+        </span>
+        <span className="container">
+          <img
+            className="compBadge"
+            src={dockerLogo}
+            alt="logo"
+            width="200px"
+            height="200px"
+          />
+        </span>
+        <span className="container">
+          <img
+            className="compBadge"
+            src={nodeLogo}
+            alt="logo"
+            width="200px"
+            height="200px"
+          />
+        </span>
+        <p className="flexbreak"></p>
+        <span className="container">
+          <img
+            className="compBadge"
+            src={pythonLogo}
+            alt="logo"
+            width="200px"
+            height="200px"
+          />
+        </span>
+        <span className="container">
+          <img
+            className="compBadge"
+            src={dotnetLogo}
+            alt="logo"
+            width="180px"
+            height="180px"
+          />
+        </span>
+        <span className="container">
+          <img
+            className="compBadge"
+            src={mongoDbLogo}
+            alt="logo"
+            width="180px"
+            height="180px"
+          />
+        </span>
+      </div>
+    </>
+  );
+}
